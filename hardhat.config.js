e215412696b96ef4e00c48b0c0aeef2c298d82b1
najienka <nemitariajienka@gmail.com>
2021-04-28 19:31:56 +0100

    Kovan network configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,20 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
 require("solidity-coverage");
 
+const ALCHEMY_API_KEY = "34UwtRY3r42lEjQMX-E_PqWvbo1KLWdA";
+const KOVAN_PRIVATE_KEY = "c13432930aa9654f4f04ec6d1581d1ec2b62ca3099ce35d5d2f985d9874d6f40";
+// const KOVAN_ACCOUNT = '0x71e48c397a37597D9813Ef1E11c60F4c5528E3de';
+// deployment docs: https://hardhat.org/tutorial/deploying-to-a-live-network.html
+
 module.exports = {
+    networks: {
+        kovan: {
+            url: `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+            accounts: [`0x${KOVAN_PRIVATE_KEY}`],
+            gasPrice: 3000000000
+        }
+    },
+
     solidity: {
         compilers: [
             {
