6e01ff567fa7797dbd2e96ae5b98b18187fd3bfd
Doug Hoyte <doug@hcsw.org>
2021-04-29 15:49:29 -0400

    "secure mode" for TestERC20 so people can't mess with us on testnets

--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -2,10 +2,12 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: UNLICENSED
 
 contract TestERC20 {
+    address owner;
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
+    bool secureMode;
 
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
@@ -13,10 +15,12 @@ contract TestERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, bool secureMode_) {
+        owner = msg.sender;
         name = name_;
         symbol = symbol_;
         decimals = decimals_;
+        secureMode = secureMode_;
     }
 
     function approve(address spender, uint256 amount) external returns (bool) {
@@ -43,20 +47,25 @@ contract TestERC20 {
 
     // Custom testing methods
 
-    function mint(address who, uint amount) external {
+    modifier secured() {
+        require(!secureMode || msg.sender == owner, "TestErc20: secure mode enabled");
+        _;
+    }
+
+    function mint(address who, uint amount) external secured {
         balanceOf[who] += amount;
         emit Transfer(address(0), who, amount);
     }
 
-    function setBalance(address who, uint newBalance) external {
+    function setBalance(address who, uint newBalance) external secured {
         balanceOf[who] = newBalance;
     }
 
-    function changeDecimals(uint8 decimals_) external {
+    function changeDecimals(uint8 decimals_) external secured {
         decimals = decimals_;
     }
 
-    function callSelfDestruct() external {
+    function callSelfDestruct() external secured {
         selfdestruct(payable(address(0)));
     }
 }
