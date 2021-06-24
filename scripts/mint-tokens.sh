3bf18b7218768f01f8379b2ca1b5387da6c5774f
najienka <nemitariajienka@gmail.com>
2021-06-24 13:27:43 +0100

    shell script with wallet address passed as argument

--- scripts/mint-tokens.sh
+++ scripts/mint-tokens.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-address=0x00E21f272A5829c842702d0bA92D99A8727D6207
-
-npx hardhat --network ropsten testtoken:mint DAI $address 1000000
+npx hardhat --network ropsten testtoken:mint DAI $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint USDC $address 1000000
+npx hardhat --network ropsten testtoken:mint USDC $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint USDT $address 1000000
+npx hardhat --network ropsten testtoken:mint USDT $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint WBTC $address 1000000
+npx hardhat --network ropsten testtoken:mint WBTC $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint UNI $address 1000000
+npx hardhat --network ropsten testtoken:mint UNI $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint COMP $address 1000000
+npx hardhat --network ropsten testtoken:mint COMP $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint REP $address 1000000
+npx hardhat --network ropsten testtoken:mint REP $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint BZRX $address 1000000
+npx hardhat --network ropsten testtoken:mint BZRX $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint DOUGH $address 1000000
+npx hardhat --network ropsten testtoken:mint DOUGH $1 1000000
 sleep 2m
-npx hardhat --network ropsten testtoken:mint CRV $address 1000000
+npx hardhat --network ropsten testtoken:mint CRV $1 1000000
 sleep 2m
