8721629380c6bbf3fce33dbba3a8a967d117b6f6
Doug Hoyte <doug@hcsw.org>
2021-07-13 09:20:49 -0400

    latest solc

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.4",
+                version: "0.8.6",
                 settings: {
                     optimizer: {
                         enabled: true,
