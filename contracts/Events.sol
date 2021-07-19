db13496bccd3cd0732039b646452eab01e40d44b
Doug Hoyte <doug@hcsw.org>
2021-07-19 10:32:21 -0400

    liquidation log

--- contracts/Events.sol
+++ contracts/Events.sol
@@ -16,6 +16,8 @@ abstract contract Events {
     event Borrow(address indexed underlying, address indexed account, uint amount);
     event Repay(address indexed underlying, address indexed account, uint amount);
 
+    event Liquidation(address indexed liquidator, address indexed violator, address indexed underlying, address collateral, uint repay, uint yield, uint healthScore, uint baseDiscount, uint discount);
+
     event AssetStatus(address indexed underlying, uint totalBalances, uint totalBorrows, uint96 reserveBalance, uint poolSize, uint interestAccumulator, int96 interestRate, uint timestamp);
 
     event ReservesConverted(address indexed underlying, address indexed recipient, uint amount);
