7b2baaf905e525b03c8d8e8c6965b173eb87cffd
Doug Hoyte <doug@hcsw.org>
2021-08-12 09:16:52 -0400

    support wrapping/unwrapping pTokens in batch transactions

--- contracts/PToken.sol
+++ contracts/PToken.sol
@@ -3,58 +3,95 @@
 pragma solidity ^0.8.0;
 
 import "./Interfaces.sol";
+import "./Utils.sol";
 
 /// @notice Protected Tokens are simple wrappers for tokens, allowing you to use tokens as collateral without permitting borrowing
 contract PToken {
-    address private immutable euler;
-    address public immutable underlying;
+    address immutable euler;
+    address immutable underlyingToken;
 
     constructor(address euler_, address underlying_) {
         euler = euler_;
-        underlying = underlying_;
+        underlyingToken = underlying_;
     }
 
 
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
-    uint public totalSupply;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowances;
+    uint totalBalances;
 
 
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
 
+    /// @notice PToken name, ie "Euler Protected DAI"
     function name() external view returns (string memory) {
-        return string(abi.encodePacked("Euler Protected ", IERC20(underlying).name()));
+        return string(abi.encodePacked("Euler Protected ", IERC20(underlyingToken).name()));
     }
 
+    /// @notice PToken symbol, ie "pDAI"
     function symbol() external view returns (string memory) {
-        return string(abi.encodePacked("p", IERC20(underlying).symbol()));
+        return string(abi.encodePacked("p", IERC20(underlyingToken).symbol()));
     }
 
+    /// @notice Number of decimals, which is same as the underlying's
     function decimals() external view returns (uint8) {
-        return IERC20(underlying).decimals();
+        return IERC20(underlyingToken).decimals();
+    }
+
+    /// @notice Address of the underlying asset
+    function underlying() external view returns (address) {
+        return underlyingToken;
     }
 
 
+    /// @notice Balance of an account's wrapped tokens
+    function balanceOf(address who) external view returns (uint) {
+        return balances[who];
+    }
+
+    /// @notice Sum of all wrapped token balances
+    function totalSupply() external view returns (uint) {
+        return totalBalances;
+    }
+
+    /// @notice Retrieve the current allowance
+    /// @param holder Address giving permission to access tokens
+    /// @param spender Trusted address
+    function allowance(address holder, address spender) external view returns (uint) {
+        return allowances[holder][spender];
+    }
+
+
+    /// @notice Transfer your own pTokens to another address
+    /// @param recipient Recipient address
+    /// @param amount Amount of wrapped token to transfer
     function transfer(address recipient, uint amount) external returns (bool) {
         return transferFrom(msg.sender, recipient, amount);
     }
 
+    /// @notice Transfer pTokens from one address to another. The euler address is automatically granted approval.
+    /// @param from This address must've approved the to address
+    /// @param recipient Recipient address
+    /// @param amount Amount to transfer
     function transferFrom(address from, address recipient, uint amount) public returns (bool) {
-        require(balanceOf[from] >= amount, "insufficient balance");
-        if (from != msg.sender && msg.sender != euler && allowance[from][msg.sender] != type(uint).max) {
-            require(allowance[from][msg.sender] >= amount, "insufficient allowance");
-            allowance[from][msg.sender] -= amount;
+        require(balances[from] >= amount, "insufficient balance");
+        if (from != msg.sender && msg.sender != euler && allowances[from][msg.sender] != type(uint).max) {
+            require(allowances[from][msg.sender] >= amount, "insufficient allowance");
+            allowances[from][msg.sender] -= amount;
         }
-        balanceOf[from] -= amount;
-        balanceOf[recipient] += amount;
+        balances[from] -= amount;
+        balances[recipient] += amount;
         emit Transfer(from, recipient, amount);
         return true;
     }
 
+    /// @notice Allow spender to access an amount of your pTokens. It is not necessary to approve the euler address.
+    /// @param spender Trusted address
+    /// @param amount Use max uint256 for "infinite" allowance
     function approve(address spender, uint amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
+        allowances[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
@@ -64,34 +101,45 @@ contract PToken {
     /// @notice Convert underlying tokens to pTokens
     /// @param amount In underlying units (which are equivalent to pToken units)
     function wrap(uint amount) external {
-        totalSupply += amount;
-        balanceOf[msg.sender] += amount;
-
-        safeTransferFrom(underlying, msg.sender, address(this), amount);
-        emit Transfer(address(0), msg.sender, amount);
+        Utils.safeTransferFrom(underlyingToken, msg.sender, address(this), amount);
+        claimSurplus(msg.sender);
     }
 
     /// @notice Convert pTokens to underlying tokens
     /// @param amount In pToken units (which are equivalent to underlying units)
     function unwrap(uint amount) external {
-        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        doUnwrap(msg.sender, amount);
+    }
 
-        totalSupply -= amount;
-        balanceOf[msg.sender] -= amount;
+    // Only callable by the euler contract:
+    function forceUnwrap(address who, uint amount) external {
+        require(msg.sender == euler, "permission denied");
+        doUnwrap(who, amount);
+    }
 
-        safeTransfer(underlying, msg.sender, amount);
-        emit Transfer(msg.sender, address(0), amount);
+    /// @notice Claim any surplus tokens held by the PToken contract. This should only be used by contracts.
+    /// @param who Beneficiary to be credited for the surplus token amount
+    function claimSurplus(address who) public {
+        uint currBalance = IERC20(underlyingToken).balanceOf(address(this));
+        require(currBalance > totalBalances, "no surplus balance to claim");
+
+        uint amount = currBalance - totalBalances;
+
+        totalBalances += amount;
+        balances[who] += amount;
+        emit Transfer(address(0), who, amount);
     }
 
 
+    // Internal shared:
 
-    function safeTransferFrom(address token, address from, address to, uint value) private {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), string(data));
-    }
+    function doUnwrap(address who, uint amount) private {
+        require(balances[who] >= amount, "insufficient balance");
 
-    function safeTransfer(address token, address to, uint value) private {
-        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), string(data));
+        totalBalances -= amount;
+        balances[who] -= amount;
+
+        Utils.safeTransfer(underlyingToken, who, amount);
+        emit Transfer(who, address(0), amount);
     }
 }
--- test/pToken.js
+++ test/pToken.js
@@ -19,17 +19,29 @@ et.testSet({
             let dpTokenAddr = await ctx.contracts.markets.underlyingToDToken(ctx.contracts.pTokens['pTST'].address);
             ctx.contracts.dTokens['dpTST'] = await ethers.getContractAt('DToken', dpTokenAddr);
         }},
-
-        { send: 'tokens.TST.approve', args: [() => ctx.contracts.pTokens.pTST.address, et.MaxUint256,], },
-        { send: 'markets.enterMarket', args: [0, () => ctx.contracts.pTokens.pTST.address], },
     ],
 })
 
+
+.test({
+    desc: "getters",
+    actions: ctx => [
+        { call: 'pTokens.pTST.name', args: [], assertEql: 'Euler Protected Test Token', },
+        { call: 'pTokens.pTST.symbol', args: [], assertEql: 'pTST', },
+        { call: 'pTokens.pTST.decimals', args: [], equals: 18, },
+        { call: 'pTokens.pTST.underlying', args: [], onResult: async (r) => {
+            et.expect(r).to.equal(ctx.contracts.tokens.TST.address);
+        }, },
+    ],
+})
 
 
 .test({
     desc: "basic wrapping",
     actions: ctx => [
+        { send: 'tokens.TST.approve', args: [() => ctx.contracts.pTokens.pTST.address, et.MaxUint256,], },
+        { send: 'markets.enterMarket', args: [0, () => ctx.contracts.pTokens.pTST.address], },
+
         { call: 'markets.getPricingConfig', args: [() => ctx.contracts.tokens.TST.address], onResult: r => {
             et.expect(r.pricingForwarded).to.equal(et.AddressZero);
         }},
@@ -41,6 +53,10 @@ et.testSet({
 
         { send: 'pTokens.pTST.wrap', args: [et.eth(11)], },
         { call: 'pTokens.pTST.balanceOf', args: [ctx.wallet.address], equals: 11, },
+        { call: 'pTokens.pTST.totalSupply', args: [], equals: 11, },
+
+        { send: 'pTokens.pTST.unwrap', args: [et.eth(11.1)], expectError: 'insufficient balance', },
+        { send: 'pTokens.pTST.claimSurplus', args: [ctx.wallet.address], expectError: 'no surplus balance to claim', },
 
         { send: 'pTokens.pTST.unwrap', args: [et.eth(1)], },
         { call: 'pTokens.pTST.balanceOf', args: [ctx.wallet.address], equals: 10, },
@@ -56,6 +72,48 @@ et.testSet({
     ],
 })
 
+
+
+.test({
+    desc: "arbitrary user can't force unwrap",
+    actions: ctx => [
+        { send: 'tokens.TST.approve', args: [() => ctx.contracts.pTokens.pTST.address, et.MaxUint256,], },
+        { send: 'markets.enterMarket', args: [0, () => ctx.contracts.pTokens.pTST.address], },
+
+        { send: 'pTokens.pTST.wrap', args: [et.eth(11)], },
+
+        { send: 'pTokens.pTST.forceUnwrap', args: [ctx.wallet.address, et.eth(1)], expectError: 'permission denied', },
+    ],
+})
+
+
+.test({
+    desc: "batch wrapping",
+    actions: ctx => [
+        { send: 'tokens.TST.approve', args: [ctx.contracts.euler.address, et.MaxUint256,], },
+
+        { action: 'sendBatch', batch: [
+            { send: 'exec.pTokenWrap', args: [ctx.contracts.tokens.TST.address, et.eth(11)], },
+            { send: 'eTokens.epTST.deposit', args: [0, et.eth(5)], },
+            { send: 'markets.enterMarket', args: [0, () => ctx.contracts.pTokens.pTST.address], },
+        ]},
+
+        { callStatic: 'exec.detailedLiquidity', args: [ctx.wallet.address], onResult: r => {
+            et.equals(r[0].status.collateralValue, 3.75, 0.001);
+        }, },
+
+        { call: 'pTokens.pTST.balanceOf', args: [ctx.wallet.address], equals: 6, },
+        { call: 'tokens.TST.balanceOf', args: [ctx.wallet.address], equals: 89, },
+
+        { action: 'sendBatch', batch: [
+            { send: 'eTokens.epTST.withdraw', args: [0, et.eth(1)], },
+            { send: 'exec.pTokenUnwrap', args: [ctx.contracts.tokens.TST.address, et.eth(1)], },
+        ]},
+
+        { call: 'pTokens.pTST.balanceOf', args: [ctx.wallet.address], equals: 6, },
+        { call: 'tokens.TST.balanceOf', args: [ctx.wallet.address], equals: 90, },
+    ],
+})
 
 
 .run();
