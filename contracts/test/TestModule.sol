ac764af6a8dae1c9f4e26aff7b5db9bee6847cf1
curtis <chiegithub@gmail.com>
2021-08-25 18:59:00 +0300

    Update TestModule.sol

--- contracts/test/TestModule.sol
+++ contracts/test/TestModule.sol
@@ -37,15 +37,46 @@ contract TestModule is BaseLogic {
         amount = decodeExternalAmount(assetCache, amount);
         transferBorrow(assetStorage, assetCache, assetStorage.dTokenAddress, from, to, amount);
     }
+    
+    // Test Log(0) -->Append log record wit no topics
+    function testEmitViaProxyNoLog(address proxyAddr) external {
+        emitViaProxy_NoTopics(proxyAddr);
+    }
 
+    // Test Log(1) -->Topic = function selector
+    function testEmitViaProxyUnTrackAverageLiquidity(address proxyAddr) external {
+        emitViaProxy_UnTrackAverageLiquidity(proxyAddr);
+    }
+
+    // Test Log(2)
+    function testEmitViaProxyTrackAverageLiquidity(address proxyAddr, address account) external {
+        emitViaProxy_TrackAverageLiquidity(proxyAddr, account);
+    }
+    // Test Log(3)
     function testEmitViaProxyTransfer(address proxyAddr, address from, address to, uint value) external {
         emitViaProxy_Transfer(proxyAddr, from, to, value);
     }
 
+    // Test Log(3)
     function testEmitViaProxyApproval(address proxyAddr, address owner, address spender, uint value) external {
         emitViaProxy_Approval(proxyAddr, owner, spender, value);
     }
-
+    
+    // Test Log(4)
+    function testEmitViaProxyLiquidation(address proxyAddr, address liquidator, address violator, 
+        address underlying, address collateral, uint repay, 
+        uint yield, uint healthScore, uint baseDiscount, uint discount)
+        external {
+            emitViaProxy_Liquidation(proxyAddr, liquidator,violator, underlying, collateral, repay, yield, healthScore, baseDiscount, discount);
+     }
+        
+    // Test Log(4)
+    function testEmitViaProxyRequestLiquidate(address proxyAddr, address liquidator,address violator, address underlying, address collateral,
+        uint repay, uint minYield)
+        external {
+            emitViaProxy_RequestLiquidate(proxyAddr, liquidator, violator, underlying, collateral, repay, minYield);
+    }
+        
     function testDispatchEmptyData() external {
         trustedSenders[address(this)].moduleId = 200;
         (bool success, bytes memory data) = address(this).call(abi.encodeWithSignature("dispatch()"));
@@ -64,5 +95,68 @@ contract TestModule is BaseLogic {
 
     function testCall() external {
         upgradeAdmin = upgradeAdmin; // suppress visibility warning
+    }
+    
+    // Emit Logs via proxies functions
+
+    function emitViaProxy_NoTopics(address proxyAddr) internal FREEMEM {
+        (bool success,) = proxyAddr.call(abi.encodePacked(
+                               uint8(0),
+                               keccak256(bytes('NoTopics()'))
+                               
+                          ));
+        require(success, "e/log-proxy-fail");
+    }
+
+    function emitViaProxy_UnTrackAverageLiquidity(address proxyAddr) internal FREEMEM {
+        (bool success,) = proxyAddr.call(abi.encodePacked(
+                               uint8(1),
+                               keccak256(bytes('UnTrackAverageLiquidity()'))
+                               
+                          ));
+        require(success, "e/log-proxy-fail");
+    }
+
+    function emitViaProxy_TrackAverageLiquidity(address proxyAddr, address account) internal FREEMEM {
+        (bool success,) = proxyAddr.call(abi.encodePacked(
+                               uint8(2),
+                               keccak256(bytes('TrackAverageLiquidity(address)')),
+                               bytes32(uint(uint160(account)))
+                               
+                          ));
+        require(success, "e/log-proxy-fail");
+    }
+
+    function emitViaProxy_Liquidation(address proxyAddr, address liquidator, address violator, address underlying, address collateral, 
+        uint repay, uint yield, uint healthScore, uint baseDiscount, uint discount) internal FREEMEM {
+        (bool success,) = proxyAddr.call(abi.encodePacked(
+                               uint8(4),
+                               keccak256(bytes('Liquidation(address,address,address,address, uint256, uint256, uint256, uint256, uint256)')),
+                               bytes32(uint(uint160(liquidator))),
+                               bytes32(uint(uint160(violator))),
+                               bytes32(uint(uint160(underlying))),
+                               bytes32(uint(uint160(collateral))),
+                               repay,
+                               yield,
+                               healthScore,
+                               baseDiscount,
+                               discount                         
+                          ));
+        require(success, "e/log-proxy-fail");
+    }
+
+    function emitViaProxy_RequestLiquidate(address proxyAddr, address liquidator, address violator, address underlying, address collateral, 
+        uint repay, uint minYield) internal FREEMEM {
+        (bool success,) = proxyAddr.call(abi.encodePacked(
+                               uint8(4),
+                               keccak256(bytes('RequestLiquidate(address,address,address,address, uint256, uint256, uint256)')),
+                               bytes32(uint(uint160(liquidator))),
+                               bytes32(uint(uint160(violator))),
+                               bytes32(uint(uint160(underlying))),
+                               bytes32(uint(uint160(collateral))),
+                               repay,
+                               minYield
+                          ));
+        require(success, "e/log-proxy-fail");
     }
 }
