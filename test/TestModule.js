4d8b9c36d9a44db8dcaf2a8493d2e5fbec9e068c
curtis <chiegithub@gmail.com>
2021-08-25 17:32:06 +0300

    Update TestModule.js

--- test/TestModule.js
+++ test/TestModule.js
@@ -1,7 +1,7 @@
 const et = require('./lib/eTestLib');
 
 et.testSet({
-    desc: "Log(0/1/2/3/4) emissions via proxy",
+    desc: "Log emissions via proxy",
 })
 
 .test({
@@ -28,7 +28,16 @@ et.testSet({
         action: 'cb', cb: async () => {
             let Proxy = await ethers.getContractFactory("Proxy");
             let proxy = await (await Proxy.deploy()).deployed();
-            let proxyAddress = proxy.address;
+
+            let errMsg;
+
+            try {
+                await (await ctx.contracts.installer.connect(ctx.wallet).installModules([proxy.address])).wait();
+            } catch (e) {
+                errMsg = e.message;
+            }
+
+            et.expect(errMsg).to.contain('reverted without a reason');
         }
     },
     ]
@@ -37,12 +46,47 @@ et.testSet({
 .test({
     desc: "Log(0) Emission",
     actions: ctx => [
-        {
-            call: 'TestModule.'
-        }
 
-        ]
-       
+        { call: 'TestModule.testEmitViaProxyNoLog', args:[proxy.address],assertEql: 'case 0'},
+
+        ],
 })  
 
+.test({
+    desc: "Log(1) Emission",
+    actions: ctx => [
+
+        { call: 'TestModule.testEmitViaProxyUnTrackAverageLiquidity', args:[proxy.address],assertEql: 'case 1'},
+
+        ],
+})
+
+.test({
+    desc: "Log(2) Emission",
+    actions: ctx => [
+
+        { call: 'TestModule.testEmitViaProxyTrackAverageLiquidity', args:[proxy.address],assertEql: 'case 2'},
+
+        ],
+})
+
+.test({
+    desc: "Log(3) Emission",
+    actions: ctx => [
+
+        { call: 'TestModule.testEmitViaProxyTransfer', args:[proxy.address],assertEql: 'case 3'},
+
+        ],
+}) 
+
+.test({
+    desc: "Log(4) Emission",
+    actions: ctx => [
+
+        { call: 'TestModule.testEmitViaProxyRequestLiquidate', args:[proxy.address],assertEql: 'case 4'},
+
+        ],
+}) 
+
+.run()
 
