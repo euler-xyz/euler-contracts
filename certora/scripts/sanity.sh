9828f90d00fbebc0addb97368708cc3dde63c016
Michael George <mdgeorge@cs.cornell.edu>
2021-09-27 12:46:44 -0400

    changed sanity to use optimization

--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -2,9 +2,12 @@
 make -C certora munged
 
 certoraRun \
-    certora/munged/modules/EToken.sol   \
-    --verify EToken:certora/spec/common.spec \
+    certora/harness/ETokenHarness.sol \
+    certora/helpers/DummyERC20A.sol   \
+    --verify ETokenHarness:certora/spec/common.spec \
+    --rule sanity                       \
     --solc solc8.0                      \
+    --solc_args '["--optimize"]' \
     --settings -t=60,-postProcessCounterExamples=true,-enableStorageAnalysis=true   \
     --msg "sanity $1"                   \
     --staging
