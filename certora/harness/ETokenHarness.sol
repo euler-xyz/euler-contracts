1c82eeb4d2c06507684d22eed55c8157608c7330
Michael George <mdgeorge@cs.cornell.edu>
2021-10-01 17:21:30 -0400

    updated scripts to take contract argument; harnessed other modules (besides eToken)

--- certora/harness/ETokenHarness.sol
+++ certora/harness/ETokenHarness.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.0;
 
-// import "./Harness.sol";
 import "../munged/modules/EToken.sol";
 
 contract ETokenHarness is EToken{
--- certora/scripts/verifyMarketsAssets.sh
+++ certora/scripts/verifyMarketsAssets.sh
@@ -1,20 +1,26 @@
-if [ -z "$1" ]
+if [ -z "$2" ]
   then
     echo "No message given!"
     echo "Usage: (from git root)"
-    echo "./certora/scripts/`basename $0` [message describing the run]"
+    echo "./certora/scripts/`basename $0` [contract] [message describing the run]"
     exit 1
 fi
 
+CONTRACT=$1
+MSG=$2
+shift 2
+
 make -C certora munged
 
-certoraRun certora/harness/ETokenHarness.sol \
+certoraRun ${CONTRACT} \
   certora/helpers/DummyERC20A.sol \
-  --verify ETokenHarness:certora/spec/MarketsAssets.spec \
+  --verify $(basename ${CONTRACT} .sol):certora/spec/MarketsAssets.spec \
   --solc solc8.0 \
   --solc_args '["--optimize"]' \
   --rule $1 \
   --short_output \
   --settings -postProcessCounterExamples=true,-enableStorageAnalysis=true \
   --loop_iter 1 --optimistic_loop \
-  --msg "Markets and Assets $1 $2" --staging
+  --msg "Markets and Assets ${MSG}" --staging \
+  $*
+
