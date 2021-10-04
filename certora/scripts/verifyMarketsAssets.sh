159eb2871e061e511daa5b3fef3d27e7400688ee
Michael George <mdgeorge@cs.cornell.edu>
2021-10-04 17:27:19 -0400

    fixed run script

--- certora/scripts/verifyMarketsAssets.sh
+++ certora/scripts/verifyMarketsAssets.sh
@@ -2,13 +2,14 @@ if [ -z "$2" ]
   then
     echo "No message given!"
     echo "Usage: (from git root)"
-    echo "./certora/scripts/`basename $0` [contract] [message describing the run]"
+    echo "./certora/scripts/`basename $0` [contract] [rule] [message describing the run]"
     exit 1
 fi
 
 CONTRACT=$1
-MSG=$2
-shift 2
+RULE=$2
+MSG=$3
+shift 3
 
 make -C certora munged
 
@@ -17,7 +18,7 @@ certoraRun ${CONTRACT} \
   --verify $(basename ${CONTRACT} .sol):certora/spec/MarketsAssets.spec \
   --solc solc8.0 \
   --solc_args '["--optimize"]' \
-  --rule $1 \
+  --rule ${RULE} \
   --short_output \
   --settings -postProcessCounterExamples=true,-enableStorageAnalysis=true \
   --loop_iter 1 --optimistic_loop \
