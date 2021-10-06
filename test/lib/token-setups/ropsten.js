f2ddd2619b624e78fd233502514f44e473a3e8a7
Doug Hoyte <doug@hcsw.org>
2021-10-05 22:39:52 -0400

    new address for USDC on ropsten, with correct decimals

--- test/lib/token-setups/ropsten.js
+++ test/lib/token-setups/ropsten.js
@@ -10,7 +10,7 @@ module.exports = {
             address: '0xB7fe2334CD47383C17bfb97B09823F11cc1A91B8',
         },
         USDC: {
-            address: '0x974d82c2A83383a3D5B6C078C3E5bBcC44EDc19F'
+            address: '0x95689Faeed6691757Df1AD48B7beA1B8Acf2dABe'
         },
         USDT: {
             address: '0x315f00239015fabc162ae87D75bdEa25f5B3D8A0'
