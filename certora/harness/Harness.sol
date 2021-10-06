19e569cc99d97e1b3543d816f0328fcb93efc235
Michael George <mdgeorge@cs.cornell.edu>
2021-10-06 10:19:34 -0400

    simplified harness dispatching and made it more robust

--- certora/harness/Harness.sol
+++ certora/harness/Harness.sol
@@ -7,75 +7,52 @@ import "../munged/modules/Markets.sol";
 // TODO as needed: import and extend other public interfaces
 contract Harness is EToken, DToken, Markets {
 
-    // This implementation chooses an arbitrary eToken and returns the
-    // corresponding state.  The message sender is taken to be msg.sender
-    address eTokenAddr;
+    // CVT will give this an arbitrary value; we use this to dispatch to the
+    // correct implementation of methods that are defined in both EToken and
+    // DToken
+    bool isDToken;
+    // This is similar to a DISPATCHER method summary, but is necessary to do
+    // here because solidity forces you to override methods defined in multiple
+    // base contracts
+
     function CALLER()
         virtual
         override(EToken,DToken)
         internal view
         returns (address underlying, AssetStorage storage assetStorage, address proxyAddr, address msgSender)
     {
-        require(eTokenAddr != address(0), "e/unrecognized-dtoken-caller");
-        msgSender    = msg.sender;
-        proxyAddr    = eTokenAddr;
-        assetStorage = eTokenLookup[proxyAddr];
-        underlying   = assetStorage.underlying;
-        require(underlying != address(0), "e/unrecognized-etoken-caller");
+        return isDToken
+            ? DToken.CALLER()
+            : EToken.CALLER();
     }
 
-    // Since allowance is provided by both DToken and EToken, we override it
-    // with a stub, and provide eTokenAllowance and dTokenAllowance methods to
-    // ensure that these methods are verified
-    //
-    // Since allowance is external, we do not need to be concerned about
-    // internal calls to allowance being overridden
     function allowance(address holder, address spender)
         virtual
         override(EToken,DToken)
         external view returns (uint)
     {
-        return 0;
+        return isDToken
+            ? DToken.allowance(holder, spender)
+            : EToken.allowance(holder, spender);
     }
 
-    function dTokenAllowance(address holder, address spender)
-        external view returns (uint)
-    {
-        return DToken.allowance(holder,spender);
-    }
-
-    function eTokenAllowance(address holder, address spender)
-        external view returns (uint)
-    {
-        return EToken.allowance(holder,spender);
-    }
-
-    // Since approve is provided by both DToken and EToken, we override it
-    // with a stub, and provide eTokenApprove and dTokenApprove methods to
-    // ensure that these methods are verified
-    //
-    // Since approve is external, we do not need to be concerned about
-    // internal calls to allowance being overridden
     function approve(address spender, uint amount)
         virtual
         override(EToken,DToken)
         external returns (bool)
     {
-        return false;
+        return isDToken
+            ? DToken.approve(spender, amount)
+            : EToken.approve(spender, amount);
     }
 
-    function dTokenApprove(address spender, uint amount)
-        external returns(bool)
+    function approveSubAccount(uint subAccountId, address spender, uint amount)
+        public returns (bool)
     {
-        return DToken.approve(spender, amount);
+        return isDToken
+            ? DToken.approveSubAccount(subAccountId, spender, amount)
+            : EToken.approveSubAccount(subAccountId, spender, amount);
     }
-
-    function eTokenApprove(address spender, uint amount)
-        external returns(bool)
-    {
-        return EToken.approve(spender, amount);
-    }
-
 }
 
 
