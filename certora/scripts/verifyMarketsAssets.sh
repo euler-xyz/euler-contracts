51b34723ebec46842ad71ceb2128dd7c2b0c0556
Michael George <mdgeorge@cs.cornell.edu>
2021-10-08 16:10:40 -0400

    made verifyMA run on all rules

--- certora/scripts/verifyMarketsAssets.sh
+++ certora/scripts/verifyMarketsAssets.sh
@@ -1,26 +1,25 @@
 spec=certora/spec/MarketsAssets.spec
 
-if [ -z "$3" ]
+if [ -z "$2" ]
   then
     echo "Incorrect number of arguments"
     echo ""
     echo "Usage: (from git root)"
-    echo "  ./certora/scripts/`basename $0` [contract] [rule] [message describing the run]"
+    echo "  ./certora/scripts/`basename $0` [contract] [message describing the run]"
     echo ""
-    echo "possible contracts:"
+    echo "Possible contracts:"
     ls -p certora/munged/modules | grep -v / | xargs basename -s .sol | sed 's/\(.*\)/  \1/g'
-    echo ""
-    echo "possible rules:"
-    # TODO: this is pretty terrible:
-    grep "^rule\|^invariant" ${spec} \
-        | sed 's/^[a-z]* \(.*\)*(.*$/  \1/'
+    # echo ""
+    # echo "possible rules:"
+    # # TODO: this is pretty terrible:
+    # grep "^rule\|^invariant" ${spec} \
+    #     | sed 's/^[a-z]* \(.*\)*(.*$/  \1/'
     exit 1
 fi
 
 contract=$1
-rule=$2
-msg=$3
-shift 3
+msg=$2
+shift 2
 
 make -C certora munged
 
@@ -29,7 +28,6 @@ certoraRun certora/munged/modules/${contract}.sol \
   --verify ${contract}:${spec} \
   --solc solc8.0 \
   --solc_args '["--optimize"]' \
-  --rule ${rule} \
   --short_output \
   --settings -postProcessCounterExamples=true,-enableStorageAnalysis=true \
   --loop_iter 1 --optimistic_loop \
