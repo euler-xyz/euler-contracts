91bce054f2a4d3a9b57981adfd2b14ccde616a8c
darek <dariushg32111@gmail.com>
2021-11-04 20:30:32 +0100

    swap config for fork node

--- scripts/fork-node.sh
+++ scripts/fork-node.sh
@@ -4,7 +4,26 @@ if [ $# -eq 0 ]; then
     echo "Usage: npm run node:fork <ALCHEMY_API_KEY> <FORK_BLOCK_NUMBER>"
     exit 1
 fi
-
+trap 'kill $PROXYPID; kill $NODEPID; exit' INT
 node $(dirname "$0")/fork-proxy.js $(hostname -I) &
-npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$1 --fork-block-number $2 --hostname $(hostname -I) --verbose 
-&& kill $!
+PROXYPID=$!
+
+mv hardhat.config.js hh.tmp
+cat <<EOT > hardhat.config.js
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1,
+      accounts: {
+         mnemonic: 'euler test test test test test test test test test test fork'
+      }
+    }
+  }
+};
+EOT
+npx hardhat node --fork https://eth-mainnet.alchemyapi.io/v2/$1 --fork-block-number $2 --hostname $(hostname -I) --verbose &
+NODEPID=$!
+
+mv hh.tmp hardhat.config.js
+tail -f /dev/null
