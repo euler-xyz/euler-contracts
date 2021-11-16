3e551bde3b6117cf7fce875205219fc3feae7772
Doug Hoyte <doug@hcsw.org>
2021-11-16 00:10:15 -0500

    natspec doc update

--- contracts/Euler.sol
+++ contracts/Euler.sol
@@ -21,11 +21,14 @@ contract Euler is Base {
 
     string public constant name = "Euler Protocol";
 
+    /// @notice Lookup the current implementation contract for a module
+    /// @param moduleId Fixed constant that refers to a module type (ie MODULEID__ETOKEN)
+    /// @return An internal address specifies the module's implementation code
     function moduleIdToImplementation(uint moduleId) external view returns (address) {
         return moduleLookup[moduleId];
     }
 
-    /// @notice Lookup a proxy that can be used to interact with a module
+    /// @notice Lookup a proxy that can be used to interact with a module (only valid for single-proxy modules)
     /// @param moduleId Fixed constant that refers to a module type (ie MODULEID__MARKETS)
     /// @return An address that should be cast to the appropriate module interface, ie IEulerMarkets(moduleIdToProxy(2))
     function moduleIdToProxy(uint moduleId) external view returns (address) {
