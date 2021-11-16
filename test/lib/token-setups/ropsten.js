a579ab455161000afd428c79121ce11e4fafead7
Doug Hoyte <doug@hcsw.org>
2021-11-16 11:34:15 -0500

    get rid of unnecessary tokens from ropsten token-setup

--- test/lib/token-setups/ropsten.js
+++ test/lib/token-setups/ropsten.js
@@ -33,53 +33,5 @@ module.exports = {
         DOUGH: {
             address: '0x6F37e457464B7D605F468FAB68F4f64ac48C2dEa'
         },
-        CRV: {
-            address: '0x86f1Dfe8F37358888D7F7D40Fa7D398b0199b6cc'
-        },
-        LINK: {
-            address: '0xDE24456D563835129266F8dc8F7891678aa5aCA6'
-        },
-        NORD: {
-            address: '0xba5dd558E654708184071B3159eD5d3f6494fad0'
-        },
-        127760: {
-            address: '0x1AE7a0A4F6Ff2Ba4449D52A22160eA8D095513fB'
-        },
-        PNK: {
-            address: '0x43136dbAF8C00Dd5779bd71d654Fe7131C377efD'
-        },
-        NFY: {
-            address: '0xa723e3012f03095493EF306f91C14d9978Aae56a'
-        },
-        AVGC: {
-            address: '0xFe33DC6Fb33F4dEA7a8331E78a4eEe326e78f7aB'
-        },
-        BAT: {
-            address: '0xf778348670eFb8572a13918974BB64aC977295bb'
-        },
-        MKR: {
-            address: '0xf049AFaCa21B0e9e076Bf0200c9511a1A6E374Ce'
-        },
-        renBTC: {
-            address: '0x9980F2f3a1D5C7D66bE05d5609a38195B0762ACC'
-        },
-        LUSD: {
-            address: '0xc2f90c2Df88eB89C56c305621776d5C38dbD7EEF'
-        },
-        MANA: {
-            address: '0x83eF02A4EC7591561574593f3a6BEf0F93885cFE'
-        },
-        CELR: {
-            address: '0x4EDe843bC2553c021564246b3821760A688C4B1f'
-        },
-        CVX: {
-            address: '0x3B57Dd5d1f2986b5De501fA9f71A1365De93B7D2'
-        },
-        renDOGE: {
-            address: '0xD64ADc0CB2FF061a867DA4437c7ebe7b6c92074D'
-        },
-        AAVE: {
-            address: '0x8230335ea1980A124678fF2297Be692cF2FDE6BE'
-        }
     },
 };
