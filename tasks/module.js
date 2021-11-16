0cfa17e3bb92ac2bc792a020c8336c61e82be99c
Doug Hoyte <doug@hcsw.org>
2021-11-16 11:37:26 -0500

    tasks for querying modules

--- tasks/module.js
+++ tasks/module.js
@@ -37,3 +37,38 @@ task("module:install")
 
         await et.taskUtils.runTx(ctx.contracts.installer.installModules(args.addrs));
     });
+
+task("module:queryInstalled")
+    .addPositionalParam("moduleId")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let impl = await ctx.contracts.euler.moduleIdToImplementation(args.moduleId);
+
+        let c = await ethers.getContractAt('BaseModule', impl);
+        let moduleGitCommit = (await c.moduleGitCommit()).substr(-40);
+
+        let proxy = await ctx.contracts.euler.moduleIdToProxy(args.moduleId);
+
+        console.log(`installed moduleId ${args.moduleId}:`);
+        console.log(`  Impl: ${impl}`);
+        console.log(`  Proxy: ${proxy}`);
+        console.log(`  moduleGitCommit: ${moduleGitCommit}`);
+    });
+
+task("module:queryImpl")
+    .addPositionalParam("addr")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let c = await ethers.getContractAt('BaseModule', args.addr);
+
+        let moduleId = (await c.moduleId()).toNumber();
+        let moduleGitCommit = (await c.moduleGitCommit()).substr(-40);
+
+        console.log(`module at address ${args.addr}:`);
+        console.log(`  moduleId: ${moduleId}`);
+        console.log(`  moduleGitCommit: ${moduleGitCommit}`);
+    });
