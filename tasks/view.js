1cffae30087e102419db898e59cdd4664dfb031a
Doug Hoyte <doug@hcsw.org>
2021-11-19 08:35:50 -0500

    task to queryIRM

--- tasks/view.js
+++ tasks/view.js
@@ -10,3 +10,17 @@ task("view")
 
         console.log(et.dumpObj(res));
     });
+
+
+task("view:queryIRM")
+    .addPositionalParam("market")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let market = await et.taskUtils.lookupToken(ctx, args.market);
+
+        let res = await ctx.contracts.eulerGeneralView.doQueryIRM({ eulerContract: ctx.contracts.euler.address, underlying: market.address, });
+
+        console.log(et.dumpObj(res));
+    });
