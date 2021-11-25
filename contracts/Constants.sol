7c84c0c089be709a5e11ef5996dfe849c8cc5eff
Doug Hoyte <doug@hcsw.org>
2021-11-24 23:07:07 -0500

    production IRM classes

--- contracts/Constants.sol
+++ contracts/Constants.sol
@@ -74,6 +74,7 @@ abstract contract Constants {
     //   Classes
     uint internal constant MODULEID__IRM_CLASS__STABLE = 2_000_500;
     uint internal constant MODULEID__IRM_CLASS__MAJOR = 2_000_501;
+    uint internal constant MODULEID__IRM_CLASS__MIDCAP = 2_000_502;
 
     // Swap types
     uint internal constant SWAP_TYPE__UNI_EXACT_INPUT_SINGLE = 1;
