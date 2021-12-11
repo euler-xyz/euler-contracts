16e34d1d2d676fc633876a31e1ff4ba5916b5c0b
Doug Hoyte <doug@hcsw.org>
2021-12-10 23:48:44 -0500

    a few convenience tokens for mainnet

--- test/lib/token-setups/mainnet.js
+++ test/lib/token-setups/mainnet.js
@@ -11,5 +11,14 @@ module.exports = {
     },
 
     existingTokens: {
+        WETH: {
+            address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        },
+        USDC: {
+            address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        },
+        DAI: {
+            address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+        },
     },
 };
