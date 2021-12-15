d6b6e100e1ff40e729d6283aab1bc1c3a325fa12
Doug Hoyte <doug@hcsw.org>
2021-12-15 15:01:14 -0500

    convenience task for querying some info from a uniswap pool

--- tasks/uniswap.js
+++ tasks/uniswap.js
@@ -6,6 +6,7 @@ const uniswapFactoryAbi = [
 const uniswapPoolAbi = [
     'function initialize(uint160 sqrtPriceX96) external',
     'function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol, bool unlocked)',
+    'function liquidity() external view returns (uint128)',
     'function observe(uint32[] calldata secondsAgos) external view returns (int56[] memory tickCumulatives, uint160[] memory liquidityCumulatives)',
 ];
 
@@ -83,4 +84,36 @@ task("uniswap:read-twap")
         let pool = new ethers.Contract(poolAddr, uniswapPoolAbi, ctx.wallet);
 
         console.log(et.dumpObj(await pool.observe([twap, 0])));
+    });
+
+task("uniswap:read-pool-info")
+    .addPositionalParam("token0")
+    .addPositionalParam("token1")
+    .addPositionalParam("fee")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let uniswapFactory = new ethers.Contract(ctx.tokenSetup.riskManagerSettings.uniswapFactory, uniswapFactoryAbi, ctx.wallet);
+
+        let token0 = await et.taskUtils.lookupToken(ctx, args.token0);
+        let token1 = await et.taskUtils.lookupToken(ctx, args.token1);
+        let fee = parseInt(args.fee);
+
+        let poolAddr = await uniswapFactory.getPool(token0.address, token1.address, fee);
+
+        if (poolAddr === et.AddressZero) {
+            throw(`No such uniswap pool`);
+        }
+
+        let pool = new ethers.Contract(poolAddr, uniswapPoolAbi, ctx.wallet);
+
+        //console.log(et.dumpObj(await pool.observe([twap, 0])));
+
+        let slot0 = await pool.slot0();
+        console.log("observationCardinality: ", slot0.observationCardinality);
+        console.log("observationCardinalityNext: ", slot0.observationCardinalityNext);
+
+        let liquidity = await pool.liquidity();
+        console.log("liquidity: ",liquidity.toString());
     });
