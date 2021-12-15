a2a1dce97d5331b7c239cde39b0785949b1094e5
Doug Hoyte <doug@hcsw.org>
2021-12-15 01:13:36 -0500

    when activating a market, choose uniswap pool with highest in-range liquidity

--- test/activateMarket.js
+++ test/activateMarket.js
@@ -102,10 +102,75 @@ et.testSet({
 })
 
 
+
 .test({
-    desc: "select third fee uniswap pool",
+    desc: "choose pool with best liquidity",
     actions: ctx => [
-        { send: 'uniswapV3Factory.setPoolAddress', args: [ctx.contracts.tokens.TST4.address, ctx.contracts.tokens.WETH.address, et.FeeAmount.MEDIUM, ctx.contracts.euler.address], },
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.MEDIUM, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [6000], },
+
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.LOW, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [9000], },
+
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.HIGH, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [7000], },
+
+        { send: 'markets.activateMarket', args: [ctx.contracts.tokens.TST4.address], },
+        { call: 'markets.getPricingConfig', args: [ctx.contracts.tokens.TST4.address], onResult: r => {
+            et.expect(r.pricingParameters).to.equal(et.FeeAmount.LOW);
+        }, },
+    ],
+})
+
+
+.test({
+    desc: "choose pool with best liquidity, 2",
+    actions: ctx => [
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.MEDIUM, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [6000], },
+
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.HIGH, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [7000], },
+
+        { send: 'markets.activateMarket', args: [ctx.contracts.tokens.TST4.address], },
+        { call: 'markets.getPricingConfig', args: [ctx.contracts.tokens.TST4.address], onResult: r => {
+            et.expect(r.pricingParameters).to.equal(et.FeeAmount.HIGH);
+        }, },
+    ],
+})
+
+
+.test({
+    desc: "choose pool with best liquidity, 3",
+    actions: ctx => [
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.MEDIUM, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [7000], },
+
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.HIGH, },
+        { send: 'uniswapPools.TST4/WETH.mockSetLiquidity', args: [6000], },
+
+        { send: 'markets.activateMarket', args: [ctx.contracts.tokens.TST4.address], },
+        { call: 'markets.getPricingConfig', args: [ctx.contracts.tokens.TST4.address], onResult: r => {
+            et.expect(r.pricingParameters).to.equal(et.FeeAmount.MEDIUM);
+        }, },
+    ],
+})
+
+
+.test({
+    desc: "pool address computation",
+    actions: ctx => [
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.MEDIUM, },
+        { action: 'createUniswapPool', pair: 'TST4/WETH', fee: et.FeeAmount.LOW, },
+
+        // Make it so that getPool(LOW) returns the pool for MEDIUM, to cause the CREATE2 address computation to fail
+
+        { action: 'cb', cb: async () => {
+            let lowPool = await ctx.contracts.uniswapV3Factory.getPool(ctx.contracts.tokens.TST4.address, ctx.contracts.tokens.WETH.address, et.FeeAmount.LOW);
+
+            await ctx.contracts.uniswapV3Factory.setPoolAddress(ctx.contracts.tokens.TST4.address, ctx.contracts.tokens.WETH.address, et.FeeAmount.MEDIUM, lowPool);
+        }, },
+
         { send: 'markets.activateMarket', args: [ctx.contracts.tokens.TST4.address], expectError: 'e/bad-uniswap-pool-addr'},
     ],
 })
