e35ad3fe6a15e7183f3ed7d00c6d0522ed3acd79
Doug Hoyte <doug@hcsw.org>
2021-12-21 06:24:04 -0500

    task for diffing etherscan verified contracts

--- tasks/verification.js
+++ tasks/verification.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const fetch = require("cross-fetch");
+const Diff = require("diff");
+
 
 task("verification:get-build")
     .addPositionalParam("contract")
@@ -9,4 +12,48 @@ task("verification:get-build")
         });
 
         fs.writeFileSync(args.outputFile, JSON.stringify(minimumBuild.input) + "\n");
+    });
+
+
+task("verification:diff-contracts")
+    .addPositionalParam("addr1")
+    .addPositionalParam("addr2")
+    .setAction(async (args) => {
+        if (!process.env.ETHERSCAN) throw(`need ETHERSCAN env variable`);
+
+        let res1 = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${args.addr1}&apikey=${process.env.ETHERSCAN}`);
+        let data1 = await res1.json();
+
+        let res2 = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${args.addr2}&apikey=${process.env.ETHERSCAN}`);
+        let data2 = await res2.json();
+
+        let extract = (data) => {
+            if (data.status !== "1") throw(`Etherscan error: ${data}`);
+
+            let source = data.result[0].SourceCode;
+            let unpacked = JSON.parse(source.substr(1, source.length - 2));
+            return unpacked;
+        };
+
+        data1 = extract(data1);
+        data2 = extract(data2);
+
+        let sourcesSeen = {};
+
+        for (let source of Object.keys(data1.sources)) {
+            sourcesSeen[source] = true;
+
+            let file1 = data1.sources[source].content;
+            let file2 = (data2.sources[source] || '').content;
+
+            if (file1 === file2) continue;
+
+            console.log(Diff.createPatch(source, file1, file2, '', ''));
+        }
+
+        for (let source of Object.keys(data2.sources)) {
+            if (sourcesSeen[source]) continue;
+
+            console.log(Diff.createPatch(source, '', data2.sources[source].content, '', ''));
+        }
     });
