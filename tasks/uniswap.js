10f699b44d4c286bb2df4522ce3700b30c7dd7fd
Doug Hoyte <doug@hcsw.org>
2021-12-27 15:59:18 -0500

    task to increase uniswap observation cardinality

--- tasks/uniswap.js
+++ tasks/uniswap.js
@@ -8,6 +8,7 @@ const uniswapPoolAbi = [
     'function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol, bool unlocked)',
     'function liquidity() external view returns (uint128)',
     'function observe(uint32[] calldata secondsAgos) external view returns (int56[] memory tickCumulatives, uint160[] memory liquidityCumulatives)',
+    'function increaseObservationCardinalityNext(uint16 observationCardinalityNext) external',
 ];
 
 task("uniswap:create-pool")
@@ -116,4 +117,30 @@ task("uniswap:read-pool-info")
 
         let liquidity = await pool.liquidity();
         console.log("liquidity: ",liquidity.toString());
+    });
+
+task("uniswap:increase-observation-cardinality")
+    .addPositionalParam("token0")
+    .addPositionalParam("token1")
+    .addPositionalParam("fee")
+    .addPositionalParam("newCardinality")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let uniswapFactory = new ethers.Contract(ctx.tokenSetup.riskManagerSettings.uniswapFactory, uniswapFactoryAbi, ctx.wallet);
+
+        let token0 = await et.taskUtils.lookupToken(ctx, args.token0);
+        let token1 = await et.taskUtils.lookupToken(ctx, args.token1);
+        let fee = parseInt(args.fee);
+
+        let poolAddr = await uniswapFactory.getPool(token0.address, token1.address, fee);
+
+        if (poolAddr === et.AddressZero) {
+            throw(`No such uniswap pool`);
+        }
+
+        let pool = new ethers.Contract(poolAddr, uniswapPoolAbi, ctx.wallet);
+
+        await et.taskUtils.runTx(pool.increaseObservationCardinalityNext(parseInt(args.newCardinality), await ctx.txOpts()));
     });
