8f6fe9088ce502ac6ed41c72f272f22f20f94d78
Doug Hoyte <doug@hcsw.org>
2022-01-18 23:28:51 -0500

    verification:diff-contract-from-repo task that lets you diff a deployed contract with the current git repo

--- tasks/verification.js
+++ tasks/verification.js
@@ -27,33 +27,51 @@ task("verification:diff-contracts")
         let res2 = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${args.addr2}&apikey=${process.env.ETHERSCAN}`);
         let data2 = await res2.json();
 
-        let extract = (data) => {
-            if (data.status !== "1") throw(`Etherscan error: ${data}`);
-
-            let source = data.result[0].SourceCode;
-            let unpacked = JSON.parse(source.substr(1, source.length - 2));
-            return unpacked;
-        };
-
-        data1 = extract(data1);
-        data2 = extract(data2);
-
-        let sourcesSeen = {};
-
-        for (let source of Object.keys(data1.sources)) {
-            sourcesSeen[source] = true;
-
-            let file1 = data1.sources[source].content;
-            let file2 = (data2.sources[source] || '').content;
-
-            if (file1 === file2) continue;
-
-            console.log(Diff.createPatch(source, file1, file2, '', ''));
-        }
-
-        for (let source of Object.keys(data2.sources)) {
-            if (sourcesSeen[source]) continue;
-
-            console.log(Diff.createPatch(source, '', data2.sources[source].content, '', ''));
-        }
+        await processDiff(extractEtherscanResult(data1), extractEtherscanResult(data2));
     });
+
+
+task("verification:diff-contract-from-repo")
+    .addPositionalParam("contract")
+    .addPositionalParam("addr")
+    .setAction(async (args) => {
+        if (!process.env.ETHERSCAN) throw(`need ETHERSCAN env variable`);
+
+        let res1 = await fetch(`https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${args.addr}&apikey=${process.env.ETHERSCAN}`);
+        let data1 = await res1.json();
+
+        let data2 = await run('verify:get-minimum-build', { sourceName: args.contract, });
+
+        await processDiff(extractEtherscanResult(data1), data2.input);
+    });
+
+
+function extractEtherscanResult(data) {
+    if (data.status !== "1") throw(`Etherscan error: ${data}`);
+
+    let source = data.result[0].SourceCode;
+    let unpacked = JSON.parse(source.substr(1, source.length - 2));
+    return unpacked;
+}
+
+
+async function processDiff(data1, data2) {
+    let sourcesSeen = {};
+
+    for (let source of Object.keys(data1.sources)) {
+        sourcesSeen[source] = true;
+
+        let file1 = data1.sources[source].content;
+        let file2 = (data2.sources[source] || '').content;
+
+        if (file1 === file2) continue;
+
+        console.log(Diff.createPatch(source, file1, file2, '', ''));
+    }
+
+    for (let source of Object.keys(data2.sources)) {
+        if (sourcesSeen[source]) continue;
+
+        console.log(Diff.createPatch(source, '', data2.sources[source].content, '', ''));
+    }
+}
