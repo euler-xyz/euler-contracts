769d2cda8fc1c747a3604abee53f70f8fcd4eaf9
Doug Hoyte <doug@hcsw.org>
2022-03-22 09:53:11 -0400

    some debugging tasks

--- tasks/view.js
+++ tasks/view.js
@@ -12,6 +12,31 @@ task("view")
     });
 
 
+task("view:account")
+    .addPositionalParam("addr")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let res = await ctx.contracts.eulerGeneralView.callStatic.doQuery({ eulerContract: ctx.contracts.euler.address, account: args.addr, markets: [], });
+
+        console.log(et.dumpObj(res));
+    });
+
+
+task("view:detailedLiquidity")
+    .addPositionalParam("addr")
+    .setAction(async (args) => {
+        const et = require("../test/lib/eTestLib");
+        const ctx = await et.getTaskCtx();
+
+        let res = await ctx.contracts.exec.callStatic.detailedLiquidity(args.addr);
+
+        console.log(et.dumpObj(res));
+    });
+
+
+
 task("view:queryIRM")
     .addPositionalParam("market")
     .setAction(async (args) => {
