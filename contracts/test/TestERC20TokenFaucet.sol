e0bb577e202e1205e2dc77daf692d49d930fcdab
najienka <nemitariajienka@gmail.com>
2022-07-21 18:20:11 +0100

    add unit tests for refactored test ERC20 token faucet

--- contracts/test/TestERC20TokenFaucet.sol
+++ contracts/test/TestERC20TokenFaucet.sol
@@ -9,24 +9,37 @@ interface IERC20 {
 }
 
 contract TestERC20TokenFaucet {
-    uint256 threshold;
-    address owner;
+    address public owner;
+    mapping(address => uint256) thresholds;
 
-    constructor(uint256 _threshold) {
-        threshold = _threshold;
+    constructor() {
         owner = msg.sender;
     }
 
-    function withdraw(address token) external {
-        uint256 decimals = IERC20(token).decimals();
-        uint256 balance = IERC20(token).balanceOf(msg.sender);
-        uint256 amount = threshold * (10 ** decimals);
-        require(balance < amount, "balance equals or exceeds faucet threshold");
-        IERC20(token).transfer(msg.sender, amount);
+    function withdraw(address underlying) external {
+        uint256 balance = IERC20(underlying).balanceOf(msg.sender);
+        uint256 amount = getThreshold(underlying);
+        require(balance < amount, "withdraw: balance not below threshold");
+        IERC20(underlying).transfer(msg.sender, amount - balance);
     }
 
-    function setThreshold(uint256 _threshold) external {
-        require(msg.sender == owner, "only callable by owner");
-        threshold = _threshold;
+    function setThreshold(address underlying, uint256 _threshold) external {
+        require(msg.sender == owner, "setThreshold: only callable by owner");
+        require(_threshold > 0, "setThreshold: threshold must be greater than zero");
+        thresholds[underlying] = _threshold;
+    }
+
+    function reduceFaucetBalance(address underlying, uint256 amount) external {
+        require(msg.sender == owner, "reduceFaucetBalance: only callable by owner");
+        if (amount == type(uint256).max) {
+            uint balance = IERC20(underlying).balanceOf(address(this));
+            IERC20(underlying).transfer(msg.sender, balance);
+        } else {
+            IERC20(underlying).transfer(msg.sender, amount);
+        }
+    }
+
+    function getThreshold(address underlying) public view returns(uint256) {
+        return thresholds[underlying];
     }
 }
