df2a4fde4023e3326edefb5ae018c2872992663c
tarieuler <tari@euler.xyz>
2023-01-26 14:18:47 +0000

    add optimism goerli deployment input file

--- test/lib/token-setups/optimism-goerli.js
+++ test/lib/token-setups/optimism-goerli.js
@@ -2,17 +2,28 @@ module.exports = {
     testing: {
         tokens: [
             {
+                name: "Euler Token",
+                symbol: "EUL",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0,
+                    borrowFactor: 0.25,
+                    borrowIsolated: true,
+                },
+            },
+            {
                 name: "Wrapped ETH",
                 symbol: "WETH",
                 decimals: 18,
                 config: {
-                    collateralFactor: 0.75,
+                    collateralFactor: 0.85,
+                    borrowFactor: 0.8,
                     borrowIsolated: false,
                 },
             },
             {
-                name: "Test Token",
-                symbol: "TST",
+                name: "Dai Stablecoin",
+                symbol: "DAI",
                 decimals: 18,
                 config: {
                     collateralFactor: 0.75,
@@ -20,9 +31,94 @@ module.exports = {
                 },
             },
             {
-                name: "Euler Token",
-                symbol: "EUL",
+                name: "bZx Protocl Token",
+                symbol: "BZRX",
                 decimals: 18,
+                config: {
+                    collateralFactor: 0.75,
+                    borrowFactor: 0.8,
+                    borrowIsolated: true,
+                },
+            },
+            {
+                name: "Curve DAO Token",
+                symbol: "CRV",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0,
+                    borrowFactor: 0.7,
+                    borrowIsolated: true,
+                },
+            },
+            {
+                name: "PieDAO DOUGH v2",
+                symbol: "DOUGH",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0.75,
+                    borrowFactor: 0.8,
+                    borrowIsolated: true,
+                },
+            },
+            {
+                name: "Reputation",
+                symbol: "REP",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0,
+                    borrowFactor: 0.76,
+                    borrowIsolated: false,
+                },
+            },
+            {
+                name: "Uniswap",
+                symbol: "UNI",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0,
+                    borrowFactor: 0.76,
+                    borrowIsolated: false,
+                },
+            },
+            {
+                name: "Tether USD",
+                symbol: "USDT",
+                decimals: 6,
+                config: {
+                    collateralFactor: 0.9,
+                    borrowFactor: 0.94,
+                    borrowIsolated: false,
+                },
+            },
+            {
+                name: "USD Coin",
+                symbol: "USDC",
+                decimals: 6,
+                config: {
+                    collateralFactor: 0.9,
+                    borrowFactor: 0.94,
+                    borrowIsolated: false,
+                },
+            },
+            {
+                name: "Wrapped BTC",
+                symbol: "WBTC",
+                decimals: 8,
+                config: {
+                    collateralFactor: 0.88,
+                    borrowFactor: 0.91,
+                    borrowIsolated: false,
+                },
+            },
+            {
+                name: "Compound",
+                symbol: "COMP",
+                decimals: 18,
+                config: {
+                    collateralFactor: 0,
+                    borrowFactor: 0.7,
+                    borrowIsolated: true,
+                },
             },
         ],
 
@@ -31,15 +127,45 @@ module.exports = {
 
         activated: [
             "WETH",
-            "TST",
+            "EUL",
+            "DAI",
+            "COMP",
+            "USDC",
+            "USDT",
+            "WBTC",
+            "UNI",
+            "REP",
+            "DOUGH",
+            "CRV",
+            "BZRX"
         ],
 
         chainlinkPrices: {
-            TST: 1
+            EUL: 0.0031974695,
+            DAI: 0.000612,
+            COMP: 0.031852903127343,
+            USDC: 0.00061877505779954,
+            USDT: 0.00062015328626584,
+            WBTC: 14.28179057601,
+            UNI: 0.0040750209099273,
+            REP: 0.0036645044321926,
+            DOUGH: 0.000039997821658434,
+            CRV: 0.0006578577613505,
+            BZRX: 0.000024967004612911,
         },
 
         chainlinkOracles: [
-            "TST",
+            "EUL",
+            "DAI",
+            "COMP",
+            "USDC",
+            "USDT",
+            "WBTC",
+            "UNI",
+            "REP",
+            "DOUGH",
+            "CRV",
+            "BZRX"
         ]
     },
 };
