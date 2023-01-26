6c4b84ff8f92cde3d4ae94904c775d5749cd1ffb
tarieuler <tari@euler.xyz>
2023-01-26 15:47:17 +0000

    update test token hardhat tasks

---
+++ test/lib/token-setups/optimismgoerli.js
@@ -0,0 +1,42 @@
+module.exports = {
+    testERC20TokenFaucet: "0x7A9921787a89A49c828667CaA7BBE48010B65D33",
+
+    existingTokens: {
+        EUL: {
+            address: "0x598419fB7cD8829E7471aFe1aFd5c2b35c9A82BC",
+        },
+        WETH: {
+            address: "0x830e930B18C795192ac20e680bcDd3ce0984B4fB",
+        },
+        DAI: {
+            address: "0x3189e190202050ae3E54f49bBD79569Ab48FF0dF",
+        },
+        BZRX: {
+            address: "0x87d71e333F01b6C6786a2374bc776FcdF8960103",
+        },
+        CRV: {
+            address: "0xc847F5f8de257D48B3c55CB868187381F6B441aA",
+        },
+        DOUGH: {
+            address: "0x2815C34e27af8Cf632049De1693FbAA0FA10F307",
+        },
+        REP: {
+            address: "0xfa8FABB91200A381fC74433B3aC3B8A6BF9eA3d0",
+        },
+        UNI: {
+            address: "0x7FbE772506080fE1261d2Df2d7dA839Ec4Dfac99",
+        },
+        USDT: {
+            address: "0x3d96928242D0585B45b1218f671875E209081fdd",
+        },
+        USDC: {
+            address: "0x90E644678b43399659010DD07259ED61E372ddb5",
+        },
+        WBTC: {
+            address: "0x1231343C8595FC2Da9C16F871e000fa69B68b9b8",
+        },
+        COMP: {
+            address: "0xCb8Ca7100Cf883769364BAd2324E710E8C17c96f",
+        },
+    },
+}
