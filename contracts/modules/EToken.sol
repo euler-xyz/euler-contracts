f67caf3ec6087e122e69d252a053fa6b1c0360f5
darek <dariushg32111@gmail.com>
2023-02-21 10:09:08 +0100

    clean up

--- contracts/modules/EToken.sol
+++ contracts/modules/EToken.sol
@@ -340,10 +340,6 @@ contract EToken is BaseLogic {
 
         checkLiquidity(from);
 
-        // // Depositing a token to an account with pre-existing debt in that token creates a self-collateralized loan
-        // // which may result in borrow isolation violation if other tokens are also borrowed on the account
-        // if (assetStorage.users[to].owed != 0) checkLiquidity(to);
-
         logAssetStatus(assetCache);
 
         return true;
