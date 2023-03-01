4f9590fa1e675b83c2a94cb11aa327a1cc354565
darek <dariushg32111@gmail.com>
2023-03-01 13:18:11 +0100

    add test for self-collateral not applied with multiple borrows

--- test/override.js
+++ test/override.js
@@ -123,12 +123,11 @@ et.testSet({
     actions: ctx => [
         // set collateral factor to 0
 
-        // { from: ctx.wallet2, send: 'dTokens.dTST3.borrow', args: [0, et.eth(1)], },
         { from: ctx.wallet2, send: 'eTokens.eTST2.mint', args: [0, et.eth(10)], },
 
 
         { call: 'exec.liquidity', args: [ctx.wallet2.address], onResult: r => {
-            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) * 1 (BF)
+            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) / 1 (BF)
             et.equals(r.collateralValue, 9.5, .001); // 20 * 0.5 (price) * 0.95 (SCF)
             et.equals(r.overrideCollateralValue, 9.5, .001); // whole collateral is in override
         }, },
@@ -145,7 +144,7 @@ et.testSet({
         ], },
 
         { call: 'exec.liquidity', args: [ctx.wallet2.address], onResult: r => {
-            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) * 1 (BF)
+            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) / 1 (BF)
             et.equals(r.collateralValue, 8, .001); // 20 * 0.5 (price) * 0.8 (CF)
             et.equals(r.overrideCollateralValue, 8, .001); // whole collateral is in override
         }, },
@@ -160,10 +159,39 @@ et.testSet({
         ], },
 
         { call: 'exec.liquidity', args: [ctx.wallet2.address], onResult: r => {
-            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) * 1 (BF)
+            et.equals(r.liabilityValue, 5, .001); // 10 * 0.5 (price) / 1 (BF)
             et.equals(r.collateralValue, 9.5, .001); // 20 * 0.5 (price) * 0.95 (SCF)
             et.equals(r.overrideCollateralValue, 9.5, .001); // whole collateral is in override
         }, },
+    ],
+})
+
+
+
+.test({
+    desc: "self-collateral override doesn't apply with multiple borrows",
+    actions: ctx => [
+        { action: 'setAssetConfig', tok: 'TST2', config: { borrowIsolated: false, collateralFactor: .7, borrowFactor: .6, }, },
+
+        { from: ctx.wallet2, send: 'eTokens.eTST2.mint', args: [0, et.eth(1)], },
+
+
+        { call: 'exec.liquidity', args: [ctx.wallet2.address], onResult: r => {
+            et.equals(r.liabilityValue, .5, .001); // 1 * 0.5 (price) / 1 (BF)
+            et.equals(r.collateralValue, 5.225, .001); // 11 * 0.5 (price) * 0.95 (SCF)
+            et.equals(r.overrideCollateralValue, 5.225, .001); // whole collateral is in override
+        }, },
+
+        // second borrow
+        { from: ctx.wallet2, send: 'dTokens.dTST3.borrow', args: [0, et.eth(2)], },
+
+        // all values counted with regular factors
+        { call: 'exec.liquidity', args: [ctx.wallet2.address], onResult: r => {
+            et.equals(r.liabilityValue, 1.833, .001); // 1 * 0.5 (price) / .6 (BF) + 2 * 0.25 (price) / 0.5 (BF)
+            et.equals(r.collateralValue, 3.85, .001); // 11 * 0.5 (price) * 0.7 (CF)
+            et.equals(r.overrideCollateralValue, 0); // no overrides
+        }, },
+
     ],
 })
 
